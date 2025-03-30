d896576ad2c025bd9dd70e41ceef5e37d8671213
Juan David <misterio562@gmail.com>
2025-03-29 22:27:43 -0500

    add CI/CD pipeline

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.routes.product import router_product 
-from app.routes.products_dynamo import router_product_dynamo 
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes.product import router_product
+from app.routes.products_dynamo import router_product_dynamo
 from app.routes.s3_routes import router_backup
 
 app = FastAPI()
@@ -8,3 +9,17 @@ app = FastAPI()
 app.include_router(router_product)
 app.include_router(router_product_dynamo)
 app.include_router(router_backup)
+
+allow_origins = [
+    "http://localhost:5173",  # React en desarrollo
+    "https://midominio.com",  # Producción
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allow_origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
+)
