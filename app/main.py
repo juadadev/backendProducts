9d6c503210b533e3e5a0a9a6a92662b87814bc9d
Juan David <misterio562@gmail.com>
2025-10-22 11:07:24 -0500

    refactor(docker): delete docker-compose.yml because now is used get out of project

--- app/main.py
+++ app/main.py
@@ -21,6 +21,7 @@ app.include_router(router_backup)
 allow_origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:80",
     "https://midominio.com",
     "*",
 ]
